bd059cca8f1944e6faafc7b2618b2fd65157c3c9
alexborsch <borsch_alex@icloud.com>
2020-01-26 14:09:13 +0200

    Initial

---
+++ main.py
@@ -0,0 +1,65 @@
+import tkinter
+from tkinter import *
+from tkinter.filedialog import asksaveasfile, askopenfile
+from tkinter.messagebox import showerror
+from tkinter import messagebox
+
+FILE_NAME = tkinter.NONE
+
+def new_file():
+	global FILE_NAME
+	FILE_NAME = "Untitled"
+	text.delete('1.0', tkinter.END)
+
+def save_file():
+	data = text.get('1.0', tkinter.END)
+	out = open(FILE_NAME, 'w')
+	out.write(data)
+	out.close()
+
+def save_as():
+	out = asksaveasfile(mode='w', defaultextension='txt')
+	data = text.get('1.0', tkinter.END)
+	try:
+		out.write(data.rstrip())
+	except Exception:
+		showerror(title="Error", message="Saving file error")
+
+def open_file():
+	global FILE_NAME
+	inp = askopenfile(mode="r")
+	if inp is None:
+		return
+	FILE_NAME = inp.name
+	data = inp.read()
+	text.delete('1.0', tkinter.END)
+	text.insert('1.0', data)
+
+def info():
+	messagebox.showinfo("Information", "CDL Notepad v.0.1\nby CoderLog\nhttps://coderlog.top")
+
+
+root = tkinter.Tk()
+root.title("CDL Notepad v.0.1")
+
+root.minsize(width=500, height=500)
+root.maxsize(width=500, height=500)
+
+text = tkinter.Text(root, width=400, height=400, wrap="word")
+scrollb = Scrollbar(root, orient=VERTICAL, command=text.yview)
+scrollb.pack(side="right", fill="y")
+text.configure(yscrollcommand=scrollb.set)
+
+text.pack()
+menuBar = tkinter.Menu(root)
+fileMenu = tkinter.Menu(menuBar)
+fileMenu.add_command(label="New", command=new_file)
+fileMenu.add_command(label="Open", command=open_file)
+fileMenu.add_command(label="Save", command=save_file)
+fileMenu.add_command(label="Save as", command=save_as)
+
+menuBar.add_cascade(label="File", menu=fileMenu)
+menuBar.add_cascade(label="Info", command=info)
+menuBar.add_cascade(label="Exit", command=root.quit)
+root.config(menu=menuBar)
+root.mainloop()
